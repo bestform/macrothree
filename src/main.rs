24d5b491e89c2b8af00fe821745946a9017ef79f
Matthias Derer <matthias.derer@esono.de>
2020-12-10 10:46:11 +0100

    add bullet texture and update player movement

--- src/main.rs
+++ src/main.rs
@@ -37,23 +37,28 @@ struct Player {
 }
 
 const DEBUG:bool = true;
-const PLAYER_WIDTH:f32 = 50.;
-const PLAYER_HEIGHT:f32 = 30.;
+
 const PLAYER_SPEED: f32 = 4.;
-const BULLET_SPEED: f32 = -6.;
-const BULLET_SIZE: f32 = 3.;
+const BOTTOM_MARGIN: f32 = 20.;
+const TOP_MARGIN: f32 = 200.;
+const PLAYER_SIZE: f32 = 100.;
+const BULLET_SPEED: f32 = -9.;
+const BULLET_SIZE: f32 = 7.;
 const STAR_DENSITY: f64 = 0.5;
 #[macroquad::main("MacroThree")]
 async fn main() {
 
     let mut player = Player {
-        pos: Vec2::new(screen_width() / 2., screen_height() - 40.),
+        pos: Vec2::new(screen_width() / 2., screen_height() - PLAYER_SIZE - BOTTOM_MARGIN),
         vel: Vec2::new(0., 0.),
         last_shot: get_time()
     };
 
     let mut bullets:Vec<Bullet> = Vec::new();
     let mut stars:Vec<Star> = Vec::new();
+
+    let ship_tx = load_texture("Resources/player_ship.png").await;
+    let bullet_tx = load_texture("Resources/bullet.png").await;
 
     loop {
         let frame_t = get_time();
@@ -71,8 +76,8 @@ async fn main() {
         // DRAW
         clear_background(BLACK);
         draw_stars(stars.clone());
-        draw_player(player);
-        draw_bullets(bullets.clone());
+        draw_player(player, ship_tx);
+        draw_bullets(bullets.clone(), bullet_tx);
         if DEBUG {
             debug::draw_debug(stars.clone(), bullets.clone());
         }
@@ -114,7 +119,7 @@ fn create_stars(stars: &mut Vec<Star>, frame_t: f64) {
     if frame_t % 1. < STAR_DENSITY {
         stars.push(Star{
             pos: Vec2::new(rand::gen_range(0., screen_width()), rand::gen_range(-20., 0.)),
-            vel: Vec2::new(0., rand::gen_range(1., 1.5)),
+            vel: Vec2::new(0., rand::gen_range(2., 2.5)),
             size: rand::gen_range(1., 2.),
             brightness: rand::gen_range(0.1, 1.)
         })
@@ -131,16 +136,27 @@ fn handle_bullet_move(bullets: &mut Vec<Bullet>) {
     }
 }
 
-fn draw_bullets(bullets: Vec<Bullet>) {
+fn draw_bullets(bullets: Vec<Bullet>, bullet_tx: Texture2D) {
     for bullet in bullets {
-        draw_circle(bullet.pos.x(), bullet.pos.y(), BULLET_SIZE, LIGHTGRAY);
+        draw_texture_ex(
+            bullet_tx,
+            bullet.pos.x() - BULLET_SIZE / 2.,
+            bullet.pos.y(),
+            WHITE,
+            DrawTextureParams{
+                dest_size: Some(Vec2::new(BULLET_SIZE, BULLET_SIZE)),
+                source: None,
+                rotation: 0.0,
+                pivot: None
+            }
+        )
     }
 }
 
 fn create_boolets(player: &mut Player, bullets: &mut Vec<Bullet>, frame_t: f64) {
     if is_key_down(KeyCode::Space) && frame_t - player.last_shot > 0.1 {
         bullets.push(Bullet{
-            pos: player.pos,
+            pos: player.pos + Vec2::new(0., 5.),
             vel: Vec2::new(0., BULLET_SPEED),
             alive: true
         });
@@ -159,9 +175,23 @@ fn handle_player_movement(player: &mut Player) {
             player.vel.set_x(PLAYER_SPEED);
         }
     }
+    if !(is_key_down(KeyCode::W) && is_key_down(KeyCode::S)) {
+        if is_key_down(KeyCode::W) {
+            player.vel.set_y(-1. * PLAYER_SPEED);
+        }
+        if is_key_down(KeyCode::S) {
+            player.vel.set_y(PLAYER_SPEED);
+        }
+    }
 
+    if player.vel.length() == 0. {
+        return;
+    }
+
+    player.vel = player.vel.normalize() * PLAYER_SPEED;
     player.pos += player.vel;
     player.pos = clampx(player.pos, 0., screen_width());
+    player.pos = clampy(player.pos, TOP_MARGIN, screen_height() - PLAYER_SIZE - BOTTOM_MARGIN);
 }
 
 fn clampx(pos: Vec2, min: f32, max: f32) -> Vec2 {
@@ -176,11 +206,30 @@ fn clampx(pos: Vec2, min: f32, max: f32) -> Vec2 {
     return new_pos;
 }
 
-fn draw_player(player: Player) {
-    draw_triangle(
-        Vec2::new(player.pos.x() -1. * PLAYER_WIDTH/2., screen_height()),
-        Vec2::new(player.pos.x() + PLAYER_WIDTH/2., screen_height()),
-        Vec2::new(player.pos.x(), screen_height() - PLAYER_HEIGHT),
-        LIGHTGRAY
+fn clampy(pos: Vec2, min: f32, max: f32) -> Vec2 {
+    let mut new_pos = pos.clone();
+    if pos.y() < min {
+        new_pos.set_y(min);
+    }
+    if pos.y() > max {
+        new_pos.set_y(max);
+    }
+
+    return new_pos;
+}
+
+fn draw_player(player: Player, ship_tx: Texture2D) {
+    draw_texture_ex(
+        ship_tx,
+        player.pos.x() - PLAYER_SIZE / 2.,
+        player.pos.y(),
+        WHITE,
+        DrawTextureParams {
+            dest_size: Some(Vec2::new(PLAYER_SIZE, PLAYER_SIZE)),
+            source: None,
+            rotation: 0.0,
+            pivot: None
+        }
     );
 }
+
